a7608b10cf67bd6c474490deae9be296953f925c
Kuba <kubamierzejewski94@gmail.com>
2019-03-10 14:03:27 +0100

    updated app

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-const result = document.querySelector('.result');
-const gramsResult = document.getElementById('gramsResult');
-const poundsResult = document.getElementById('poundsResult');
-const ouncesResult = document.getElementById('ouncesResult');
+const result = document.querySelector(".result");
+const gramsResult = document.getElementById("gramsResult");
+const poundsResult = document.getElementById("poundsResult");
+const ouncesResult = document.getElementById("ouncesResult");
 
-result.style.visibility = 'hidden';
+result.style.visibility = "hidden";
 
-document.getElementById('kgInput').addEventListener('input', function(e) {
-	result.style.visibility = 'visible';
-	let kg = e.target.value;
-	gramsResult.innerText = kg * 1000;
-	poundsResult.innerText = kg * 2.2046;
-	ouncesResult.innerText = kg * 35.2739619;
-
+document.getElementById("kgInput").addEventListener("input", function(e) {
+  result.style.visibility = "visible";
+  let kg = e.target.value;
+  gramsResult.innerText = kg * 1000;
+  poundsResult.innerText = kg * 2.2046;
+  ouncesResult.innerText = kg * 35.2739619;
 });
